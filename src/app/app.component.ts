ece403601e425af894b75172f8c7b9b3aca71fbf
Sam Guerrero <samguerrero@Sams-MacBook-Pro.local>
2019-03-18 11:53:59 -0700

    Practicing angular component and typescript interactions
    practice adding buttons to html and adding functionality to type script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NinjaGold';
+  total = 0;
+  activity = [];
+
+  addFarm(){
+    console.log("farm")
+    let value = Math.floor((Math.random() * 5) + 1);
+    this.total += value;
+    this.activity.push(`You earned ${value} at the farm`);
+  }
+
+  addCave() {
+    console.log("cave")
+    let value = Math.floor((Math.random() * (10-5+1)) + 5);
+    this.total += value;
+    this.activity.push(`You earned ${value} at the cave`);
+  }
+
+  addHouse() {
+    console.log("house")
+    let value = Math.floor((Math.random() * (15-7+1)) + 7);
+    this.total += value;
+    this.activity.push(`You earned ${value} at the house`);
+  }
+8
+  addCasino() {
+    console.log("casino")
+    let value = Math.floor((Math.random() * (100+100+1)) + -100);
+    this.total += value;
+    this.activity.push(value > 0 ? `You earned ${value} at the casino` : `You lost ${value} at the casino`)
+  }
+
 }
